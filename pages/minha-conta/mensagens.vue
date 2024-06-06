038512d1bce01dd78bdf4a5ce7e4427ff9644690
ViniciusFachini <vinicius.fachini01@gmail.com>
2024-06-05 22:44:04 -0300

    Update mensagens.vue

--- pages/minha-conta/mensagens.vue
+++ pages/minha-conta/mensagens.vue
@@ -3,41 +3,66 @@
     <div class="page-wrapper container">
       <div class="chat-wrapper">
         <aside class="chat-list">
+          <h3 class="chat-list__title">Minhas mensagens <span class="quantity">({{ chats.length }})</span></h3>
           <ul>
-            <li
-              class="chat-item"
-              v-for="chat in chats"
-              :key="chat.chat_partner.chat_partner_id"
-            >
+            <li class="chat-item" v-for="(chat, index) in chats" :key="chat.chat_partner.chat_partner_id"
+              @click="selectChat(chat)">
+              <div class="item-wrapper"
+                :class="index == 0 ? 'top' : index == chats.length - 1 ? 'bottom' : 'middle', chat.messages.unreadMessages && chat.messages.unreadMessages >= 1 ? 'has-unseen-messages' : ''">
+                <figure class="chat-item__user-image">
+                  <img :src="chat.chat_partner.chat_partner_profile_picture
+                    ? treatUserImage(
+                      chat.chat_partner.chat_partner_profile_picture
+                    )
+                    : 'https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png'
+                    " alt="" />
+                </figure>
+                <figcaption class="chat-item__user-info">
+                  <span class="chat-item__user-info--unseen-badge"
+                    v-if="chat.messages.unreadMessages && chat.messages.unreadMessages >= 1">{{
+                      chat.messages.unreadMessages }} Mensagens não lidas</span>
+                  <span class="chat-item__user-info--name">
+                    {{ chat.chat_partner.chat_partner_name }}
+                  </span>
+                  <span class="chat-item__user-info--content">
+                    {{ truncateText(chat.messages.lastMessage.content) }}
+                  </span>
+                  <div class="chat-item__user-info--timestamp">
+                    {{ formatDate(chat.messages.lastMessage.timestamp) }}
+                  </div>
+                </figcaption>
+                <!-- <p>Chat with: {{ chat.chat_partner_id }}</p> -->
+              </div>
               <!-- {{ chat.chat_partner }} -->
-              <figure class="chat-item__user-image">
-                <img
-                  :src="
-                    chat.chat_partner.chat_partner_profile_picture
-                      ? treatUserImage(
-                          chat.chat_partner.chat_partner_profile_picture
-                        )
-                      : 'https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png'
-                  "
-                  alt=""
-                />
-              </figure>
-              <figcaption class="chat-item__user-info">
-                <span class="chat-item__user-info--name">
-                  {{ chat.chat_partner.chat_partner_name }}
-                </span>
-                <span class="chat-item__user-info--content">
-                  {{ truncateText(chat.messages.lastMessage.content) }}
-                </span>
-                <div class="chat-item__user-info--timestamp">
-                  {{ formatDate(chat.messages.lastMessage.timestamp) }}
-                </div>
-              </figcaption>
-              <!-- <p>Chat with: {{ chat.chat_partner_id }}</p> -->
             </li>
           </ul>
         </aside>
-        <section class="chat-content"></section>
+        <section class="chat-content">
+          <article class="has-chat" v-if="selectedChat && selectedChat[0]">
+            <div class="chat-content__user">
+              <figure class="chat-item__user-image">
+                <img :src="selectedChat[0] && selectedChat[0].sender_profile_picture ? treatUserImage(selectedChat[0].sender_profile_picture) : 'https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png'" alt="" />
+              </figure>
+              <figcaption class="chat-item__user-info">
+                <span class="chat-item__user-info--name" v-if="selectedChat[0] && selectedChat[0].sender_name">
+                  {{ selectedChat[0].sender_name }}
+                </span>
+              </figcaption>
+            </div>
+            <ul class="messages">
+              <li class="message" v-for="(item, index) in selectedChat" :class="getBubbleClass(item)" :key="item.id">
+                <span class="chat-bubble">
+                  {{ item.content }}
+                </span>
+              </li>
+            </ul>
+            <form @submit.prevent="sendMessage($event)"></form>
+            <input type="text">
+          </article>
+          <div class="chat-not-selected" v-else>
+            Seleciona um chat ae kkkkk
+          </div>
+        </section>
       </div>
     </div>
   </main>
@@ -51,6 +76,7 @@ export default {
       message: "",
       socket: null,
       authenticated: false,
+      selectedChat: {}
     };
   },
   async created() {
@@ -83,6 +109,37 @@ export default {
         console.error("Error fetching last chats:", error);
       }
     },
+    getBubbleClass(bubble) {
+      const currentUserId = this.session.value.user.id
+      if (bubble.sender_id == currentUserId) {
+        return 'sent'
+      } else {
+        return 'received'
+      }
+    },
+    selectChat(chat) {
+      // Mark the clicked chat as selected
+      this.chats.forEach(c => {
+        c.selected = (c === chat);
+      });
+
+      // Make API request for the selected chat
+      this.fetchChatMessages(chat.chat_partner.chat_partner_id);
+    },
+    async fetchChatMessages(chatPartnerId) {
+      try {
+        // Make the API request using the chatPartnerId
+        // Example:
+        const recieverId = this.session.value.user.id
+        console.log(chatPartnerId, this.session.value.user.id)
+        const response = await this.$fetchInfoAuthenticated(`messages/${chatPartnerId}/${recieverId}`, "GET");
+        this.selectedChat = await response
+        console.log(this.selectedChat)
+        // Handle the response accordingly
+      } catch (error) {
+        console.error("Error fetching chat messages:", error);
+      }
+    },
     formatDate(dateString) {
       const date = new Date(dateString);
       const now = new Date();
@@ -103,12 +160,13 @@ export default {
         ? text.substring(0, maxLength) + "..."
         : text;
     },
-    sendMessage() {
+    sendMessage(reciever) {
       this.socket.emit("sendMessage", {
-        receiver_id: "user2",
+        receiver_id: reciever,
         content: this.message,
       }); // Replace 'user2' with the actual receiver ID
       this.message = "";
+      console.log(reciever)
     },
     async treatUserName(name) {
       const nameParts = name.split(" ");
@@ -154,7 +212,6 @@ export default {
   },
   async mounted() {
     const response = await this.checkUserAuth();
-    console.log(response);
     if (response) {
       this.authenticated = true;
     } else {
@@ -168,27 +225,82 @@ export default {
 main {
   background-color: #f1f1f1;
 }
+
 .page-wrapper {
+  padding-block: 40px;
+
   .chat-wrapper {
     width: 100%;
     margin: 0 auto;
     display: grid;
     grid-template-columns: 400px 1fr;
+    align-items: start;
+    gap: 20px;
+
     .chat-list {
+      &__title {
+        font-size: 24px;
+        padding: 16px 12px;
+        font-weight: 500;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .quantity {
+          font-weight: 700;
+          font-size: 20px;
+        }
+      }
+
       ul {
+        background-color: white;
+        border-radius: 8px;
+
         .chat-item {
-          width: 100%;
-          display: flex;
-          align-items: center;
-          justify-content: start;
-          gap: 12px;
+
+          .item-wrapper {
+            width: 100%;
+            display: flex;
+            align-items: center;
+            padding: 20px;
+            justify-content: start;
+            gap: 12px;
+            transition: all .2s linear;
+            cursor: pointer;
+
+            &.top {
+              border-radius: 10px 10px 0 0;
+            }
+
+            &.bottom {
+              border-radius: 0 0 10px 10px;
+            }
+
+            &.middle {
+              border-radius: 0;
+            }
+
+            &.has-unseen-messages {
+              background-color: hsl(352, 93%, 90%);
+            }
+
+            &:hover {
+              background-color: #f83a53;
+              color: white;
+            }
+          }
+
+
           &__user-image {
             max-width: 50px;
+
             img {
               width: 100%;
               border-radius: 50%;
+              background-color: white;
             }
           }
+
           &__user-info {
             display: flex;
             flex-direction: column;
@@ -197,16 +309,93 @@ main {
             justify-content: center;
             gap: 4px;
             position: relative;
+
             &--timestamp {
               position: absolute;
               top: 50%;
               transform: translateY(-50%);
               right: 0;
             }
+
+            &--unseen-badge {
+              font-size: 14px;
+            }
+
+            &--name {
+              font-size: 16px;
+              font-weight: 500;
+            }
+
+            &--content {
+              font-size: 12px;
+            }
+
+          }
+        }
+      }
+    }
+
+    .chat-content {
+      background-color: white;
+      border-radius: 8px;
+      height: 100%;
+      min-height: 600px;
+      max-height: 600px;
+
+      &__user {
+        display: flex;
+        align-items: center;
+        justify-content: start;
+        gap: 16px;
+        padding: 16px 32px;
+        border-bottom: 1px solid #eee;
+        figure {
+          max-width: 50px;
+          img {
+            border-radius: 50%;
+            background-color: white;
+            cursor: pointer;
+          }
+        }
+        figcaption{
+          span {
+            font-size: 24px;
+          }
+        }
+      }
+      
+      .has-chat {
+        height: 100%;
+      }
+
+      .messages {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: end;
+        height: 100%;
+        padding: 8px;
+        max-height: calc(100% - 83px);
+
+        .message {
+          display: flex;
+          align-items: center;
+          width: 100%;
+
+          &.received {
+            justify-content: start;
+          }
+
+          &.sent {
+            justify-content: end;
+          }
+
+          .chat-bubble {
+            width: 100%;
+            max-width: 400px;
           }
         }
       }
     }
   }
-}
-</style>
+}</style>
